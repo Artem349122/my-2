2f5ae131074aa2150721527554bcd9b6fc19213f
Artem349122 <153762931+Artem349122@users.noreply.github.com>
2024-02-20 14:15:04 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,7 +5,38 @@ $(function() {
         infinite: true,
         slidesToShow: 6,
         slidesToScroll: 6,
-        arrows: false
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1250,
+                settings: {
+                  slidesToShow: 5,
+                  slidesToScroll: 5
+                }
+              },
+              {
+                breakpoint: 1040,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 4
+                }
+              },
+              {
+                breakpoint: 830,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3
+                }
+              },
+              {
+                breakpoint: 650,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              }
+        ]
+        
         
     });
 
@@ -24,7 +55,23 @@ $(function() {
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 4,
-        arrows: false
+        arrows: false,
+        responsive: [
+          {
+              breakpoint: 950,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+              }
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            }
+        ]
     });
 
     $('#slideLeftComp').on('click',function(){
@@ -34,9 +81,17 @@ $(function() {
     $('#slideRightComp').on('click',function(){
         sliderCompany.slick('slickNext');
     });
+//================Add to Cart========================
+
+    $('.btn-prod').on("click", function(){
+        $('.cart span').addClass('summ--active')
+        let count = $('#count').html();
+        count = Number(count) + 1;
+         $('#count').html(count);
+        });
 
 });
-
+//================Active To Click=====================
 let footer=document.getElementById("navF");
 let btns=footer.getElementsByClassName("foot-list__link");
 
@@ -65,7 +120,7 @@ for (let i = 0; i < btns.length; i++) {
     });
 }
 
-     
+//==================Scroll to Top========================
 let t;
 function up() {
 	let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
@@ -76,4 +131,30 @@ function up() {
 	return false;
 }
 
+//================================Modal============================
+$(function() {
+    $('[data-modal]').on('click',function(event){
+        event.preventDefault();
 
+        let modal=$(this).data('modal');
+        $('body').addClass('no-scroll');
+        $(modal).addClass('show');
+    });
+
+    $('[data-modal-close]').on('click',function(event){
+        event.preventDefault();
+
+        let modal=$(this).parents('.mod');
+        $('body').removeClass('no-scroll');
+        $(modal).removeClass('show');
+    });
+
+    $('.mod').on('click',function(){
+        $('body').removeClass('no-scroll');
+        $(this).removeClass('show');
+    });
+
+    $('.mod__inner').on('click',function(event){
+        event.stopPropagation();
+    });
+});
